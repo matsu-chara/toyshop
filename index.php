ed139bfa65ffa9739970659f2c7db76d03a86dff
matsu-chara <matsuy00@gmail.com>
2014-07-26 02:59:15 +0900

    add toy model

--- index.php
+++ index.php
@@ -1,29 +1,32 @@
 <?php
+use Silex\Application;
+use Model\Toy;
+
 require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Silex\Application();
 $app['debug'] = true;
 
 $toys = array(
-    '00001' => array(
-        'name' => 'Racing Car',
-        'quantity' => '53',
-        'description' => '...',
-        'image' => 'racing_car.jpg',
+    '00001' => new Toy(
+        'Racing Car',
+        '53',
+        '...',
+        'racing_car.jpg'
     ),
-    '00002' => array(
-        'name' => 'Raspberry Pi',
-        'quantity' => '13',
-        'description' => '...',
-        'image' => 'raspberry_pi.jpg',
-    ),
+    '00002' => new Toy(
+        'Raspberry Pi',
+        '13',
+        '...',
+        'raspberry_pi.jpg'
+    )
 );
 
 $app->get('/', function () use ($toys) {
     return json_encode($toys);
 });
 
-$app->get('/{stockcode}', function (Silex\Application $app, $stockcode) use ($toys) {
+$app->get('/{stockcode}', function (Application $app, $stockcode) use ($toys) {
     if (!isset($toys[$stockcode])) {
         $app->abort(404, "Stockcode {$stockcode} does not exist.");
     }
