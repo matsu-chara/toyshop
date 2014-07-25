b1b68e4802c3f145fe026c145523f1c5b2a531f3
matsu-chara <matsuy00@gmail.com>
2014-07-26 02:59:26 +0900

    add toy model

---
+++ src/Model/Toy.php
@@ -0,0 +1,22 @@
+<?php
+namespace Model;
+
+class Toy
+{
+    public $name;
+    public $quantity;
+    public $description;
+    public $image;
+
+    public function __construct(
+        $name,
+        $quantity,
+        $description = '',
+        $image = ''
+    ) {
+        $this->name = $name;
+        $this->quantity = $quantity;
+        $this->description = $description;
+        $this->image = $image;
+    }
+}
